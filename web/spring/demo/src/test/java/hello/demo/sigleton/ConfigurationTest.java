7ed04ad8159d87de05cfa7ed7d2a8f708a6070b5
latte <stary7stars@gmail.com>
2021-03-12 20:52:00 +0900

    [Spring]6.Singleton

---
+++ web/spring/demo/src/test/java/hello/demo/sigleton/ConfigurationTest.java
@@ -0,0 +1,41 @@
+package hello.demo.sigleton;
+
+import hello.demo.AppConfig;
+import hello.demo.member.Member;
+import hello.demo.member.MemberRepository;
+import hello.demo.member.MemberService;
+import hello.demo.member.MemberServiceImpl;
+import hello.demo.order.OrderServiceImpl;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+public class ConfigurationTest {
+
+    @Test
+    void configurationTest() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+
+        MemberServiceImpl memberService = ac.getBean("memberService", MemberServiceImpl.class);
+        OrderServiceImpl orderService = ac.getBean("orderService", OrderServiceImpl.class);
+        MemberRepository memberRepository = ac.getBean("memberRepository", MemberRepository.class);
+
+        MemberRepository memberRepository1 = memberService.getMemberRepository();
+        MemberRepository memberRepository2 = orderService.getMemberRepository();
+
+        System.out.println("memberRepository1 = " + memberRepository1);
+        System.out.println("memberRepository2 = " + memberRepository2);
+        System.out.println("memberRepository = " + memberRepository);
+
+        Assertions.assertThat(memberRepository1).isSameAs(memberRepository2);
+    }
+
+    @Test
+    void configurationDeep() {
+        ApplicationContext ac = new AnnotationConfigApplicationContext(AppConfig.class);
+        AppConfig bean = ac.getBean(AppConfig.class);
+
+        System.out.println("bean = " + bean.getClass());
+    }
+}
